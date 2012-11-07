ee912ca3137cc9cb105bfdbc56b7ce60b5ad5a1e
Nicolas Paris <nicolas.caen@gmail.com>
2012-11-07 13:05:39 +0100

    Loader

---
+++ lib/Loader.php
@@ -0,0 +1,34 @@
+<?php
+
+    require_once 'Zend/Loader/StandardAutoloader.php';
+    use Zend\Loader\StandardAutoloader;
+    use Zend\Config\Reader\Yaml;
+
+    class Loader {
+
+        static public function load() {
+            $autoLoader = new StandardAutoloader(array(
+                'prefixes' => array(
+                    'MyVendor' => __DIR__ . '/MyVendor',
+                ),
+                'namespaces' => array(
+                    'MyNamespace' => __DIR__ . '/MyNamespace',
+                ),
+                'fallback_autoloader' => true,
+            ));
+
+            // register our StandardAutoloader with the SPL autoloader
+            $autoLoader->register();
+
+
+
+
+
+            $reader = new Zend\Config\Reader\Yaml();
+            $data   = $reader->fromFile('config.yaml');
+var_dump($data);
+
+        }
+
+
+    }
