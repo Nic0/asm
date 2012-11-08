1626a1d71253d7034e010d357f1b1618b2c8ed85
Nicolas Paris <nicolas.caen@gmail.com>
2012-11-08 15:54:11 +0100

    table configurable

--- controller/StaticPageController.php
+++ controller/StaticPageController.php
@@ -2,12 +2,14 @@
 
     require_once '../lib/Controller.php';
     require_once '../model/ZabbixEvent.php';
-
     class StaticPageController extends Controller {
 
         public function home () {
             $zEvent = new ZabbixEvent();
             $data = array('zabbix' => $zEvent->getLast());
+
+            $data['config'] = AsmConfig::getConfig();
+
             $this->render($data);
         }
     }
