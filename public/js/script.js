a8b89eb2e6404ccb91891c64cf9b4cbd3959234f
Nicolas Paris <nicolas.caen@gmail.com>
2012-11-13 09:42:06 +0100

    update time in navbar

--- public/js/script.js
+++ public/js/script.js
@@ -4,14 +4,19 @@
 
 function ajax_update_glpi (config) {
     setInterval(function() {
-        $('#glpi').load('/glpi/update');
+        $('#glpi').load('/glpi/update', update_time());
     }, config.js.glpi.interval * 1000);
 }
 
 function ajax_update_zabbix (config) {
     setInterval(function() {
-        $('#zabbix').load('/zabbix/update');
+        $('#zabbix').load('/zabbix/update', update_time());
     }, config.js.zabbix.interval * 1000);
+}
+
+function update_time () {
+    var currentTime = new Date()
+    $('a[name=update]').html(currentTime.getHours() + ':' + currentTime.getMinutes());
 }
 
 
@@ -40,50 +45,5 @@ function ajax_config () {
 
 jQuery(document).ready(function($) {
     ajax_config();
-
-
-
-        // init the Sortables
-        $(".column").sortable({
-            connectWith:    $(".column")
-        ,   placeholder:    'widget-placeholder'
-        ,   cursor:         'move'
-        //  use a helper-clone that is append to 'body' so is not 'contained' by a pane
-        ,   helper:         function (evt, ui) { return $(ui).clone().appendTo('body').show(); }
-        ,   over:           function (evt, ui) {
-                                var
-                                    $target_UL  = $(ui.placeholder).parent()
-                                ,   targetWidth = $target_UL.width()
-                                ,   helperWidth = ui.helper.width()
-                                ,   padding     = parseInt( ui.helper.css('paddingLeft') )
-                                                + parseInt( ui.helper.css('paddingRight') )
-                                                + parseInt( ui.helper.css('borderLeftWidth') )
-                                                + parseInt( ui.helper.css('borderRightWidth') )
-                                ;
-                                //if (( (helperWidth + padding) - targetWidth ) > 20)
-                                    ui.helper
-                                        .height('auto')
-                                        .width( targetWidth - padding )
-                                    ;
-                            }
-        });
-/*
-        $("#draggable").draggable({
-        //  use a helper-clone that is append to 'body' so is not 'contained' by a pane
-            helper: function () { return $(this).clone().appendTo('body').css('zIndex',5).show(); }
-        ,   cursor: 'move'
-        });
-
-        $('#droppable').droppable({
-           accept:  '#draggable'
-        ,  drop:    function () { alert('The Draggable was Dropped!'); }
-        });
-*/
-
-
-
-
-
-
 });
 
