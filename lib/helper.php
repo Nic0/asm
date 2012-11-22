e3201d2255f1798474988e6f2abb72133146a092
Nicolas Paris <nicolas.caen@gmail.com>
2012-11-22 11:40:59 +0100

    fixing hours:min for ldap bad password

--- lib/helper.php
+++ lib/helper.php
@@ -83,16 +83,9 @@
             return '0000-00-00';
         }
 
-        $secsAfterADEpoch = $ad_date / (10000000);
-        $AD2Unix=((1970-1601) * 365 - 3 + round((1970-1601)/4) ) * 86400;
+        $ad_date=$ad_date /10000000-11644473600;
 
-        // Why -3 ?
-        // "If the year is the last year of a century, eg. 1700, 1800, 1900, 2000,
-        // then it is only a leap year if it is exactly divisible by 400.
-        // Therefore, 1900 wasn't a leap year but 2000 was."
-
-        $unixTimeStamp=intval($secsAfterADEpoch-$AD2Unix);
-        $myDate = date("d/m/Y H:m", $unixTimeStamp); // formatted date
+        $myDate = date("d/m/Y H:i", $ad_date); // formatted date
 
         return $myDate;
     }
