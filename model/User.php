107a30cd7fc31bd05f7fc68b1b75c189c50554ca
Nicolas Paris <nicolas.caen@gmail.com>
2012-11-28 11:17:34 +0100

    some more comments for models

--- model/User.php
+++ model/User.php
@@ -6,8 +6,20 @@
     use Zend\Authentication\AuthenticationService;
     use Zend\Authentication\Adapter\Ldap as AuthAdapter;
 
+    /**
+     * @brief Classe pour un utilisateur
+     *
+     * L'authentification se fait sur LDAP, mais la gestion de rôle dans
+     * l'application se fait sur la base de données phpasm
+     */
     class User extends PhpAsmModel {
 
+        /**
+         * @brief Authentification par LDAP s'appuyant sur Zend2
+         * @param  string $username Login de l'utilisateur
+         * @param  string $password Mot de passe de l'utilisateur
+         * @return int Valeur relatif à l'authentification et Zend
+         */
         public function authenticate ($username, $password) {
             $auth = new AuthenticationService();
 
@@ -34,6 +46,11 @@
             return $result;
         }
 
+        /**
+         * @brief Obtient le rôle enregistré dans la base phpasm en fonction de son login
+         * @param  string $login Login de l'utilisateur
+         * @return string        Rôle de l'utilisateur (admin, null)
+         */
         public function getRole ($login) {
             $this->sql = new Sql($this->adapter);
             $select = $this->sql->select();
--- model/ZabbixHost.php
+++ model/ZabbixHost.php
@@ -4,12 +4,20 @@
 
     use Zend\Db\Sql\Sql;
 
-
+    /**
+     * @brief Gestion d'un Host par Zabbix
+     */
     class ZabbixHost extends ZabbixModel {
 
+        /** @brief identifiant de l'host */
         public $hostid;
+        /** @brief Nom de l'host */
         public $host;
 
+        /**
+         * @brief Obtient tout les hosts surveillés par Zabbix
+         * @return array tableau d'objet ZabbixHost comportant la totalité des hosts
+         */
         public function getAll() {
             $this->sql = new Sql($this->adapter);
             $select = $this->sql->select();
