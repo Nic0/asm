b6fb8b8e3a64bc8a0d0bfadd723a52d95e773c51
Nicolas Paris <nicolas.caen@gmail.com>
2012-12-03 15:21:07 +0100

    functionnal testing

--- tests/acceptance/LogoutCept.php
+++ tests/acceptance/LogoutCept.php
@@ -1,8 +1,8 @@
  <?php
     $I = new WebGuy($scenario);
-    $I->wantTo('Unloged user should not see dashboard');
+    $I->wantTo('check if I dont have the dashboard link as a visitor');
     $I->amOnPage('/logout');
     $I->amOnPage('/');
     $I->see('Login');
-    $I->dontSee('Dashboard');
+    $I->dontSeeLink('Dashboard');
 ?>
