9ced87e1e4ba6f1b52a54844bba48ef16b457a0c
Nicolas Paris <nicolas.caen@gmail.com>
2012-12-12 09:22:13 +0100

    simpleimage done

--- controller/GroupController.php
+++ controller/GroupController.php
@@ -2,6 +2,7 @@
 
     require_once '../lib/Controller.php';
     require_once '../model/Group.php';
+    require_once '../vendor/SimpleImage.php';
 
     /**
      * @brief Controlleur pour les groupes
@@ -12,6 +13,15 @@
             if (isPost()) {
                 $group = new Group();
                 $group = $group->create($_POST);
+
+
+                move_uploaded_file($_FILES["logo"]["tmp_name"],
+                                   "img/upload/" . $_FILES["logo"]["name"]);
+
+                $image = new SimpleImage();
+                $image->load("img/upload/" . $_FILES["logo"]["name"]);
+                $image->resizeToHeight(48);
+                $image->save("img/upload/" . $_FILES["logo"]["name"]);
 
                 unset($group->sous_group);
                 unset($group->state);
